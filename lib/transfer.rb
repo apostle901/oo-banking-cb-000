381cf331ae339b24f4ba3e48d2ba188fed4d8630
Jawann <jawann@gmail.com>
2017-12-08 04:54:20 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,9 @@
 class Transfer
   # your code here
+  attr_reader :name
+  attr_accessor :account, :amount
+  
+  def initialize(name, account, amount)
+    @name, @bank_account, @amount = name, account, amount
+  end
 end
