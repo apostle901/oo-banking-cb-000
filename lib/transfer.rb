520239967e52f3a8bc7aeccbc2d145d0561cc3b0
Jawann <jawann@gmail.com>
2017-12-08 05:03:20 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,6 +12,7 @@ class Transfer
   end
 
   def execute_transaction
+    return "Transaction rejected. Please check your account balance." if !valid?
     return if @status == 'complete'
     @sender.deposit(@amount * -1)
     @receiver.deposit(@amount)
